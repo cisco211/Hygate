957824a60ade0e6a99b66e9d28d492c75873d23e
C!$C0^211 (Toni Schumacher) <TjSBMD1810@yahoo.de>
2026-01-26 21:00:43 +0100

    FEAT: Added Hygate_Auto from hytale plugin template;

---
+++ mods/Hygate_Auto/src/main/java/com/example/exampleplugin/ExamplePlugin.java
@@ -0,0 +1,21 @@
+package com.example.exampleplugin;
+
+import javax.annotation.Nonnull; // C: Ok i need that, so JavaPluginInit does not produce a warning.
+
+import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+public class ExamplePlugin extends JavaPlugin {
+    private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
+
+    public ExamplePlugin(@Nonnull JavaPluginInit init) {
+        super(init);
+        LOGGER.atInfo().log("Hello from %s version %s", this.getName(), this.getManifest().getVersion().toString());
+    }
+
+    @Override
+    protected void setup() {
+        this.getCommandRegistry().registerCommand(new ExampleCommand(this.getName(), this.getManifest().getVersion().toString()));
+    }
+}
