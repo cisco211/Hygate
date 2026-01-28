c59895a5fae4da11510af657596e8b5242d09c6f
C!$C0^211 (Toni Schumacher) <TjSBMD1810@yahoo.de>
2026-01-28 11:33:08 +0100

    FEAT: Added colored chat output to HygateAuto;

---
+++ mods/HygateAuto/src/main/java/de/cisco211/hygate/hygateauto/MessageEx.java
@@ -0,0 +1,83 @@
+package de.cisco211.hygate.hygateauto;
+
+import java.util.Objects;
+
+import javax.annotation.Nonnull;
+
+import com.hypixel.hytale.server.core.Message;
+
+/**
+ * <b>MessageEx</b>
+ * <br/>
+ * Helper class with convenient static functions.
+ */
+public class MessageEx
+{
+	private MessageEx()
+	{}
+
+	/**
+	 * <b>Language key</b>
+	 * <br/>
+	 * File name used for all embedded translations in this plugin.
+	 */
+	protected static final String LANG_KEY = "hygate_auto";
+
+	/**
+	 * <b>Lng</b>
+	 * <br/>
+	 * Translate into language by key.
+	 * @param key String
+	 * @return @Nonnull Message
+	 */
+	public static @Nonnull Message lng(String key)
+	{
+		return Message.translation(LANG_KEY + "." + Objects.requireNonNull(key));
+	}
+
+	/**
+	 * <b>LngErr</b>
+	 * <br/>
+	 * Translate into language as error.
+	 * @param key String
+	 * @return @Nonnull Message
+	 */
+	public static @Nonnull Message lngErr(String key)
+	{
+		return lng(key).color("#CC0000");
+	}
+
+	/**
+	 * <b>LngInfo</b>
+	 * <br/>
+	 * Translate into language as info.
+	 */
+	public static @Nonnull Message lngInfo(String key)
+	{
+		return lng(key).color("#AAAAFF");
+	}
+
+	/**
+	 * <b>LngWarn</b>
+	 * <br/>
+	 * Translate into language as warning.
+	 * @param key String
+	 * @return @Nonnull Message
+	 */
+	public static @Nonnull Message lngWarn(String key)
+	{
+		return lng(key).color("#FF6600");
+	}
+
+	/**
+	 * <b>LngOk</b>
+	 * <br/>
+	 * Translate into language as success.
+	 * @param key String
+	 * @return @Nonnull Message
+	 */
+	public static @Nonnull Message lngOk(String key)
+	{
+		return lng(key).color("#008800");
+	}
+}
